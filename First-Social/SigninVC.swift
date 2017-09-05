f75231cfc1a7dd249d2727827e4fb9a4a0797e04
Neven <neven@Nevens-MacBook-Pro-2.local>
2017-09-05 22:48:17 +0800

    first-setup

---
+++ First-Social/SigninVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  First-Social
+//
+//  Created by Neven on 05/09/2017.
+//  Copyright © 2017 Neven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-First-Social/Pods-First-Social-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_First_Social : NSObject
+@end
+@implementation PodsDummy_Pods_First_Social
+@end
