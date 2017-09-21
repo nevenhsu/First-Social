493b4d1b798cf47fe829db65ce1eecc3f7e88134
Neven <neven@Nevens-MacBook-Pro-2.local>
2017-09-21 23:32:26 +0800

    setup firebase uid

---
+++ First-Social/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  First-Social
+//
+//  Created by Neven on 20/09/2017.
+//  Copyright © 2017 Neven. All rights reserved.
+//
+
+import Foundation
---
+++ First-Social/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  First-Social
+//
+//  Created by Neven on 20/09/2017.
+//  Copyright © 2017 Neven. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
